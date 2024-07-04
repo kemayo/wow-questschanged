e6f91f7b69e76d2531f55ea5137743331e0d3383
David Lynch <kemayo@gmail.com>
2024-07-04 15:53:04 -0500

    Clean up some deprecated wow-api calls
    Fixes #3

--- addon.lua
+++ addon.lua
@@ -1,5 +1,5 @@
 local myname, ns = ...
-local myfullname = GetAddOnMetadata(myname, "Title")
+local myfullname = C_AddOns.GetAddOnMetadata(myname, "Title")
 
 local icon = LibStub("LibDBIcon-1.0", true)
 
--- vignettes.lua
+++ vignettes.lua
@@ -1,5 +1,5 @@
 local myname, ns = ...
-local myfullname = GetAddOnMetadata(myname, "Title")
+local myfullname = C_AddOns.GetAddOnMetadata(myname, "Title")
 
 local function VignettePosition(vignetteGUID)
     local uiMapID = C_Map.GetBestMapForUnit('player')
